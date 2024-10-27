9ab142324c4cde1ddf40696ca7ea827426c64bc4
Jack Myrick <contact@jackmyrick.design>
2024-10-27 19:53:30 +0000

    Made it so whatever activity you press first plays rather than always defaulting to 1

--- Assets/Scripts/ActivityManager.cs
+++ Assets/Scripts/ActivityManager.cs
@@ -12,32 +12,89 @@ public class ActivityManager : MonoBehaviour
     private string variableName;
 
     [SerializeField]
-    private float minValue;
-
-    [SerializeField]
-    private float maxValue;
-
-    [SerializeField]
     private float curValue;
 
     [SerializeField]
     private StudioEventEmitter studioEventEmitter;
 
+    [SerializeField]
+    private int CurrentActivity;
+
+    [Header("Activity Perameter Triggers")]
+
+    [SerializeField]
+    private GameObject ActivityTrigger1;
+
+    [SerializeField]
+    private GameObject ActivityTrigger2;
+
+    [SerializeField]
+    private GameObject ActivityTrigger3;
+
+    private bool playing = false;
+
     public void Activity1()
     {
-        studioEventEmitter.EventInstance.setParameterByName(variableName, 0);
-        curValue = 0;
+       // studioEventEmitter.EventInstance.setParameterByName(variableName, 10 / 100);
+
+        curValue = 10;
+        CurrentActivity = 1;
+
+        ActivityTrigger1.gameObject.SetActive(true);
+        ActivityTrigger2.gameObject.SetActive(false);
+        ActivityTrigger3.gameObject.SetActive(false);
+
+        if (!playing)
+        {
+            studioEventEmitter.Play();
+            studioEventEmitter.gameObject.SetActive(true);
+            playing = true;
+        }
     }
 
     public void Activity2()
     {
-        studioEventEmitter.EventInstance.setParameterByName(variableName, 45);
+        //studioEventEmitter.EventInstance.setParameterByName(variableName, 45 / 100);
         curValue = 45;
+        CurrentActivity = 2;
+
+        ActivityTrigger1.gameObject.SetActive(false);
+        ActivityTrigger2.gameObject.SetActive(true);
+        ActivityTrigger3.gameObject.SetActive(false);
+
+        if (!playing)
+        {
+            studioEventEmitter.Play();
+            studioEventEmitter.gameObject.SetActive(true);
+            playing = true;
+        }
     }
 
     public void Activity3()
     {
-        studioEventEmitter.EventInstance.setParameterByName(variableName, 75);
+        //studioEventEmitter.EventInstance.setParameterByName(variableName, 75 / 100);
         curValue = 75;
+        CurrentActivity = 3;
+
+        ActivityTrigger1.gameObject.SetActive(false);
+        ActivityTrigger2.gameObject.SetActive(false);
+        ActivityTrigger3.gameObject.SetActive(true);
+
+        if (!playing)
+        {
+            studioEventEmitter.Play();
+            studioEventEmitter.gameObject.SetActive(true);
+            playing = true;
+        }
+    }
+
+    public void Stop()
+    {
+        if (playing)
+        {
+            studioEventEmitter.Stop();
+            studioEventEmitter.gameObject.SetActive(false);
+            playing = false;
+        }
     }
 }
