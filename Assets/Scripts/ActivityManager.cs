4b4c8600b537fe9b83272e1de648fd43eef7481d
Jack Myrick <contact@jackmyrick.design>
2024-10-27 22:46:36 +0000

    made the activity buttons also toggle off the music if pressed again

--- Assets/Scripts/ActivityManager.cs
+++ Assets/Scripts/ActivityManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using UnityEngine.Events;
 
 public class ActivityManager : MonoBehaviour
 {
@@ -24,42 +25,101 @@ public class ActivityManager : MonoBehaviour
     [SerializeField]
     private GameObject ActivityTrigger3;
 
+    [Header("Activity Enabled")]
+
+    [SerializeField]
+    private UnityEvent On1;
+
+    [SerializeField]
+    private UnityEvent On2;
+
+    [SerializeField]
+    private UnityEvent On3;
+
+    [Header("Activity Disabled")]
+
+    [SerializeField]
+    private UnityEvent Off1;
+
+    [SerializeField]
+    private UnityEvent Off2;
+
+    [SerializeField]
+    private UnityEvent Off3;
+
     private bool playing = false;
+
+    private bool Act1 = false;
+    private bool Act2 = false;
+    private bool Act3 = false;
 
     public void Activity1()
     {
-        ActivityTrigger1.GetComponent<FMODUnity.StudioGlobalParameterTrigger>().TriggerParameters();
-
-        if (!playing)
+        if (!Act1)
         {
-            studioEventEmitter.Play();
+            ActivityTrigger1.GetComponent<FMODUnity.StudioGlobalParameterTrigger>().TriggerParameters();
+            Act1 = true;
+            On1.Invoke();
             playing = true;
-            Debug.Log("Music Playing = " + playing);
+            Playing();
+        }
+        else
+        {
+            Act1 = false;
+            Off1.Invoke();
+            playing = false;
+            Playing();
         }
     }
 
     public void Activity2()
     {
-        ActivityTrigger2.GetComponent<FMODUnity.StudioGlobalParameterTrigger>().TriggerParameters();
-
-        if (!playing)
+        if (!Act2)
         {
-            studioEventEmitter.Play();
+            ActivityTrigger2.GetComponent<FMODUnity.StudioGlobalParameterTrigger>().TriggerParameters();
+            Act2 = true;
+            On2.Invoke();
             playing = true;
-            Debug.Log("Music Playing = " + playing);
+            Playing();
+        }
+        else
+        {
+            Act2 = false;
+            Off2.Invoke();
+            playing= false;
+            Playing();
         }
     }
 
     public void Activity3()
     {
-        ActivityTrigger3.GetComponent<FMODUnity.StudioGlobalParameterTrigger>().TriggerParameters();
-
-        if (!playing)
+        if (!Act3)
         {
-            studioEventEmitter.Play();
+            ActivityTrigger3.GetComponent<FMODUnity.StudioGlobalParameterTrigger>().TriggerParameters();
+            Act3 = true;
+            On3.Invoke();
             playing = true;
-            Debug.Log("Music Playing = " + playing);
+            Playing();
         }
+        else
+        {
+            Act3 = false;
+            Off3.Invoke();
+            playing= false;
+            Playing();
+        }
+    }
+
+    private void Playing()
+    {
+        if (playing)
+        {
+            studioEventEmitter.Play();     }
+        else
+        {
+            studioEventEmitter.Stop();
+        }
+        Debug.Log("Music Playing = " + playing);
     }
 
     public void Stop()
