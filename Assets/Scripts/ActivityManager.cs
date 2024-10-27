47a15b5cdbf4b6edca0d5ab878280d99ba662b4e
Jack Myrick <contact@jackmyrick.design>
2024-10-27 18:09:13 +0000

    script so music changes when you select different activities

--- Assets/Scripts/ActivityManager.cs
+++ Assets/Scripts/ActivityManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class ActivityManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject ActivityPerameterObject;
+
+    [SerializeField]
+    private string variableName;
+
+    [SerializeField]
+    private float minValue;
+
+    [SerializeField]
+    private float maxValue;
+
+    [SerializeField]
+    private float curValue;
+
+    [SerializeField]
+    private StudioEventEmitter studioEventEmitter;
+
+    public void Explore()
     {
-        
+        studioEventEmitter.EventInstance.setParameterByName(variableName, 0);
+        curValue = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Combat()
     {
-        
+        studioEventEmitter.EventInstance.setParameterByName(variableName, 45);
+        curValue = 45;
+    }
+
+    public void Victory()
+    {
+        studioEventEmitter.EventInstance.setParameterByName(variableName, 75);
+        curValue = 75;
     }
 }
