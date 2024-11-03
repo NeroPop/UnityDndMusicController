1c8bd859857bc99ddeab44ab8d278d4a085166bc
Jack Myrick <contact@jackmyrick.design>
2024-11-03 23:27:57 +0000

    added functionality to the volume slider

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using FMODUnity;
 
 public class VolumeManager : MonoBehaviour
 {
     [SerializeField]
     private Slider ActivityVolumeSlider;
 
+    [SerializeField]
+    private GameObject PerameterTrigger;
 
+    [SerializeField]
+    private string ActivityVolName;
+
+    private float ActivityVolume;
+    public float ActivityCurVolume;
+
+    public void ActivityVolumeChange()
+    {
+        ActivityVolume = ActivityVolumeSlider.value;
+
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName(ActivityVolName, ActivityVolume);
+        PerameterTrigger.GetComponent<FMODUnity.StudioGlobalParameterTrigger>().TriggerParameters();
+        FMODUnity.RuntimeManager.StudioSystem.getParameterByName(ActivityVolName, out ActivityCurVolume);
+    }
 }
