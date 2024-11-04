38986a5cdd0cf1f01a1899a6316a685b59f9de5c
Jack Myrick <contact@jackmyrick.design>
2024-11-04 19:08:09 +0000

    button sprites now change when selecting between different buttons

--- Assets/Scripts/UnityActivityManager.cs
+++ Assets/Scripts/UnityActivityManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -19,9 +20,63 @@ public class UnityActivityManager : MonoBehaviour
 
     [Header("Audio Mixers")]
 
-    public AudioMixerGroup ExploreMixer;
+    public AudioMixerGroup[] ActivityMixers;
 
-    public AudioMixerGroup CombatMixer;
+    [Header("Buttons")]
 
-    public AudioMixerGroup VictoryMixer;
+    [SerializeField]
+    private Sprite ButtonDefaultSprite;
+
+    [SerializeField]
+    private Sprite ButtonSelectedSprite;
+
+    [SerializeField]
+    private GameObject[] ActivityButtons;
+
+    //[Header("Fade")]
+    //public float FadeDuration = 5;
+
+    [Header("Debugging")]
+
+    private bool playing = false;
+
+    public int Act = 0;
+    public int PrevAct;
+
+    public float CurrentTime;
+    public float FadeDuration = 5;
+    private float FadeTime;
+    private float FadeoutTime = 0;
+    private float CurFadeTime;
+
+
+    public void TriggerActivity(int ActivityNumber)
+    {
+        PrevAct = Act;
+
+        //Runs if the button hasn't already been pressed
+        if (Act != ActivityNumber)
+        {
+            //sets the current activity
+            Act = ActivityNumber;
+            playing = true;
+
+            //changes the button sprites
+            ActivityButtons[Act-1].GetComponent<Image>().sprite = ButtonSelectedSprite;
+            ActivityButtons[PrevAct - 1].GetComponent<Image>().sprite = ButtonDefaultSprite;
+        }
+
+        //Runs if the button has already been pressed
+        else
+        {
+            ActivityButtons[Act-1].GetComponent<Image>().sprite = ButtonDefaultSprite;
+            playing = false;
+        }
+    }
+
+    private void Update()
+    {
+        //sets current time and is used to measure how long the fades have been going on for.
+        CurrentTime = CurrentTime + Time.deltaTime;
+    }
 }
