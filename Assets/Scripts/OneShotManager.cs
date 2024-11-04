f761ee6a5bfc11b9c3826306c2d3afa094a77226
Jack Myrick <contact@jackmyrick.design>
2024-11-04 00:38:28 +0000

    Created functional script that allows you to play different oneshots by changing an int

--- Assets/Scripts/OneShotManager.cs
+++ Assets/Scripts/OneShotManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 using UnityEngine.UI;
+using System;
 
 public class OneShotManager : MonoBehaviour
 {
@@ -15,5 +16,17 @@ public class OneShotManager : MonoBehaviour
     private GameObject PerameterTrigger;
 
     [SerializeField]
-    public string OneShotVarName;
+    public string OneShotPeramName;
+
+    [SerializeField]
+    private float CurOneshot;
+
+    public void playOneShot(int OneShotNumber)
+    {
+        RuntimeManager.StudioSystem.setParameterByName(OneShotPeramName, OneShotNumber);
+        PerameterTrigger.GetComponent<StudioGlobalParameterTrigger>().TriggerParameters();
+        studioEventEmitter.Play();
+        RuntimeManager.StudioSystem.getParameterByName(OneShotPeramName, out CurOneshot);
+        Debug.Log("Playing OneShot " + CurOneshot);
+    }
 }
