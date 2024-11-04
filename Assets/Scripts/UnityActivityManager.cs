00da4dfdfbb56e36dd62168775792ffc01b4d31a
Jack Myrick <contact@jackmyrick.design>
2024-11-04 19:56:31 +0000

    added audio fadeout when switching between tracks

--- Assets/Scripts/UnityActivityManager.cs
+++ Assets/Scripts/UnityActivityManager.cs
@@ -48,13 +48,15 @@ public class UnityActivityManager : MonoBehaviour
     public float CurrentTime;
     public float FadeDuration = 5;
     private float VolumeLevel;
-    private float FadeoutTime = 0;
+    private float PreVolumeLevel;
+    private float FadeoutVolume = 0;
     private float CurFadeTime;
 
 
     public void TriggerActivity(int ActivityNumber)
     {
         PrevAct = Act;
+        PreVolumeLevel = VolumeLevel;
 
         //Runs if the button hasn't already been pressed
         if (Act != ActivityNumber)
@@ -63,8 +65,10 @@ public class UnityActivityManager : MonoBehaviour
             Act = ActivityNumber;
             playing = true;
 
-            //Fade stuff
+            //Resets fading
             CurrentTime = 0;
+            FadeoutVolume = FadeDuration;
+            CurFadeTime = PreVolumeLevel;
 
             //changes the button sprites
             ActivityButtons[Act-1].GetComponent<Image>().sprite = ButtonSelectedSprite;
@@ -100,8 +104,17 @@ public class UnityActivityManager : MonoBehaviour
                 //VolumeLevel sets the volume between 0-1, going up as time gets closer to fade duration.
                 VolumeLevel = CurrentTime / FadeDuration;
 
-                //Sets the activity volume to FadeTime
+                //Sets the activity volume to VolumeLevel
                 ActivityMixers[Act - 1].audioMixer.SetFloat(MixerVolNames[Act-1], Mathf.Log10(VolumeLevel) * 20);
+            }
+
+            if (FadeoutVolume > 0)
+            {
+                //Sets FadeoutVolume to the opposite of that of VolumeLevel, also taking the current fade time into account.
+                FadeoutVolume = (CurFadeTime - (CurrentTime / FadeDuration));
+
+                //sets the previous activity volume to FadeoutVolume which decreases over time.
+                ActivityMixers[PrevAct - 1].audioMixer.SetFloat(MixerVolNames[PrevAct - 1], Mathf.Log10(FadeoutVolume) * 20);
             }
         }
     }
