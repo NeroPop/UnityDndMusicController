d0997caac77c4dc3d747b28e7b2738df6a9a1d0c
Jack Myrick <contact@jackmyrick.design>
2024-11-05 00:13:05 +0000

    created new manager scripts

--- Assets/Scripts/OneShotManager.cs
+++ Assets/Scripts/OneShotManager.cs
@@ -1,32 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using FMODUnity;
-using UnityEngine.UI;
-using System;
 
 public class OneShotManager : MonoBehaviour
 {
-    [Header("FMOD References")]
 
-    [SerializeField]
-    private StudioEventEmitter studioEventEmitter;
-
-    [SerializeField]
-    private GameObject PerameterTrigger;
-
-    [SerializeField]
-    public string OneShotPeramName;
-
-    [SerializeField]
-    private float CurOneshot;
-
-    public void playOneShot(int OneShotNumber)
-    {
-        RuntimeManager.StudioSystem.setParameterByName(OneShotPeramName, OneShotNumber);
-        PerameterTrigger.GetComponent<StudioGlobalParameterTrigger>().TriggerParameters();
-        studioEventEmitter.Play();
-        RuntimeManager.StudioSystem.getParameterByName(OneShotPeramName, out CurOneshot);
-        //Debug.Log("Playing OneShot " + CurOneshot);
-    }
 }
--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -1,55 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using FMODUnity;
 
 public class VolumeManager : MonoBehaviour
 {
-    [Header("FMOD References")]
 
-    [SerializeField]
-    private GameObject PerameterTrigger;
-
-    [SerializeField]
-    private string ActivityVolName;
-
-    [SerializeField]
-    private string OneshotVolName;
-
-    [Header("UI Elements")]
-
-    [SerializeField]
-    private Slider ActivityVolumeSlider;
-
-    [SerializeField]
-    private Slider OneshotVolumeSlider;
-
-    [Header("Debug")]
-
-    private float ActivityVolume;
-    private float OneshotVolume;
-
-    private float ActivityCurVolume;
-    private float OneshotCurVolume;
-
-    public void ActivityVolumeChange()
-    {
-        ActivityVolume = ActivityVolumeSlider.value;
-
-        RuntimeManager.StudioSystem.setParameterByName(ActivityVolName, ActivityVolume);
-        PerameterTrigger.GetComponent<StudioGlobalParameterTrigger>().TriggerParameters();
-
-        RuntimeManager.StudioSystem.getParameterByName(ActivityVolName, out ActivityCurVolume);
-    }
-
-    public void OneshotVolumeChange()
-    {
-        OneshotVolume = OneshotVolumeSlider.value;
-
-        RuntimeManager.StudioSystem.setParameterByName(OneshotVolName, OneshotVolume);
-        PerameterTrigger.GetComponent<StudioGlobalParameterTrigger>().TriggerParameters();
-
-        RuntimeManager.StudioSystem.getParameterByName(OneshotVolName, out OneshotCurVolume);
-    }
 }
