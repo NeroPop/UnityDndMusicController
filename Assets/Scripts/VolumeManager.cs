66774865e78f986ca1b798f6db82a1d45e527509
Jack Myrick <contact@jackmyrick.design>
2024-11-05 00:27:27 +0000

    setup references in VolumeManager script

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -1,8 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
+    [Header("UI Elements")]
 
+    [SerializeField]
+    private Slider ActivityVolumeSlider;
+
+    [SerializeField]
+    private Slider OneshotVolumeSlider;
+
+    [Header("Volume Mixers")]
+
+    [SerializeField]
+    private AudioMixerGroup MasterMixer;
+    [SerializeField]
+    private string MasterVolName;
+
+    [SerializeField]
+    private AudioMixerGroup ActivitiesMixer;
+    [SerializeField]
+    private string ActivitiesVolName;
+
+    [SerializeField]
+    private AudioMixerGroup OneshotsMixer;
+    [SerializeField]
+    private string OneshotsVolName;
+
+    [Header("Debug")]
+
+    private float ActivityVolume;
+    private float OneshotVolume;
+
+    private float ActivityCurVolume;
+    private float OneshotCurVolume;
+
+    public void ActivityVolumeChange()
+    {
+        ActivityVolume = ActivityVolumeSlider.value;
+        ActivitiesMixer.audioMixer.SetFloat(ActivitiesVolName, ActivityVolume);
+    }
 }
