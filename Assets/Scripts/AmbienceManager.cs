c3c908d8292650081eb040d4b370bf1632e7d027
Jack Myrick <contact@jackmyrick.design>
2024-11-06 01:19:30 +0000

    selecting an ambience button will now toggle the button state

--- Assets/Scripts/AmbienceManager.cs
+++ Assets/Scripts/AmbienceManager.cs
@@ -1,31 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AmbienceManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] AmbientAudioSources;
 
+    [SerializeField]
+    private GameObject[] AmbientButtons;
+
     public float FadeDuration = 5;
 
     private int Amb;
     public bool Pressed = false;
 
-    public void PlayAmbientAudio(int AmbientAudio)
+    public void PlayAmbientAudio(int AmbientButton)
     {
         //Ensures the button always plays a sound even if there's more buttons than sound.
-        if (AmbientAudio >= AmbientAudioSources.Length)
+        if (AmbientButton >= AmbientAudioSources.Length)
         {
             Amb = AmbientAudioSources.Length - 1;
         }
-        else if (AmbientAudio < AmbientAudioSources.Length)
+        else if (AmbientButton < AmbientAudioSources.Length)
         {
-            Amb = AmbientAudio;
+            Amb = AmbientButton;
         }
 
         //Triggers the ambient sound to begin or stop.
         AmbientAudioSources[Amb].GetComponent<AmbientController>().TriggerAmbient();
+
+        AmbientButtons[AmbientButton].GetComponent<ButtonToggle>().ToggleButton();
     }
 }
