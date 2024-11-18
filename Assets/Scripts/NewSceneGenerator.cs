2525a869b8b0022a52bb9550e5a1bb8c70735256
Jack Myrick <contact@jackmyrick.design>
2024-11-18 19:31:16 +0000

    you can now name the new scene

--- Assets/Scripts/NewSceneGenerator.cs
+++ Assets/Scripts/NewSceneGenerator.cs
@@ -1,26 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NewSceneGenerator : MonoBehaviour
 {
+    [Header("Prefabs")]
+
     [SerializeField]
     private GameObject ScenePrefab;
 
     [SerializeField]
     private GameObject SceneButtonPrefab;
 
+    [Header("References")]
+
     [SerializeField]
     private GameObject ScenesButtonGroup;
 
+    [SerializeField]
+    private TMP_InputField SceneNameInput;
+
+    [Header("New Scene Properties")]
+
+    [SerializeField]
+    private string NewSceneName;
+
+    [SerializeField]
+    private int NewSceneInt;
+
     public void NewScene()
     {
+        NewSceneName = SceneNameInput.text;
+
         // Instantiate the SceneButtonPrefab as a child of ScenesButtonGroup
         GameObject newSceneButton = Instantiate(SceneButtonPrefab, ScenesButtonGroup.transform);
+        //Changes the button name text
+        newSceneButton.GetComponentInChildren<TMP_Text>().text = NewSceneName;
 
-        // Optionally, reset the local position, rotation, or scale if needed
-        newSceneButton.transform.localPosition = Vector3.zero; // Adjust as per requirement
-        newSceneButton.transform.localRotation = Quaternion.identity; // Reset rotation if necessary
-        newSceneButton.transform.localScale = Vector3.one; // Reset scale if necessary
+       //newSceneButton.name = NewSceneName; not sure this is necessary (changes the button name in unity hierarchy)
     }
 }
