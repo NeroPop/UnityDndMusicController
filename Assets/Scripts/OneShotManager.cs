2f0638af53ce166c48d6ae024460f3c0d59dd274
Jack Myrick <contact@jackmyrick.design>
2024-11-26 19:59:19 +0000

    added some new references in the OneShotManager script

--- Assets/Scripts/OneShotManager.cs
+++ Assets/Scripts/OneShotManager.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class OneShotManager : MonoBehaviour
 {
+    [Header("Prefabs")]
+
+    [SerializeField]
+    private GameObject OneShotPrefab;
+
+    [SerializeField]
+    private GameObject OneShotButtonPrefab;
+
+    [Header("References")]
+
+    [SerializeField]
+    private GameObject OneShotButtonGroup;
+
+    [Header("Audio")]
+
     [SerializeField]
     private AudioSource[] OneshotAudioSources;
 
