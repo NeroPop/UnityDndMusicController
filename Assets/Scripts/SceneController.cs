2ac554c1ba997f1d09001d322a16c37826c3fbde
Jack Myrick <contact@jackmyrick.design>
2024-12-18 00:27:15 +0000

    Added Ambience Manager stuff to the Scene Controller Script

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -22,24 +22,38 @@ public class SceneController : MonoBehaviour
 
     private void Start()
     {
-        SceneManager = GameObject.Find("/SceneManager");
+        SceneManager = GameObject.Find("/SceneManager"); //Finds Scene Manager
+
+        //Sets the Scene Name for various scripts
         MusicManager.GetComponent<customAudioClipLoader>().Scene = SceneName;
         MusicManager.GetComponent<OneshotFileSelector>().SceneName = SceneName;
+        MusicManager.GetComponent<AmbienceFileSelector>().SceneName = SceneName;
         MusicManager.GetComponent<OneShotManager>().SceneName = SceneName;
+        MusicManager.GetComponent<AmbienceManager>().SceneName = SceneName;
         SceneTitleTxt.text = SceneName;
+
+        //Adds listener to the scene title button for Scene switching
         SceneTitle.onClick.AddListener(SceneManager.GetComponent<SceneManager>().SelectSceneUI);
         Debug.Log("Scene " + SceneName + " has started");
     }
-    public void ActivateScene()
+    public void ActivateScene() //Triggered when a scene is activated
     {
         MusicManager.GetComponent<UnityActivityManager>().NewScene();
+
+        //Gives Oneshot Manager the Scene name and triggers it to load existing Oneshots
         MusicManager.GetComponent<OneShotManager>().SceneName = SceneName;
         MusicManager.GetComponent<OneShotManager>().LoadExistingWavFiles();
+
+        //Gives Ambience Manager the Scene name and triggers it to load existing Ambience
+        MusicManager.GetComponent<AmbienceManager>().SceneName = SceneName;
+        MusicManager.GetComponent<AmbienceManager>().LoadExistingWavFiles();
     }
 
-    public void DeactivateScene()
+    public void DeactivateScene() //Triggered when the scene is deactivated
     {
+        //Tells the Oneshots and Ambience managers that they need to be cleaned when loaded back in
         MusicManager.GetComponent<OneShotManager>().clean = false;
+        MusicManager.GetComponent<AmbienceManager>().clean = false;
         Debug.Log("Scene Deactivated");
     }
 }
