4cc3ba6695dc7baa3b0db83c112182e9f9228121
Jack Myrick <contact@jackmyrick.design>
2024-12-18 00:16:49 +0000

    added Ambience UI to customisation interface

--- Assets/Scripts/CustomisationInterface.cs
+++ Assets/Scripts/CustomisationInterface.cs
@@ -15,15 +15,31 @@ public class CustomisationInterface : MonoBehaviour
     [SerializeField] private GameObject OneshotButtonDone;
     [SerializeField] private GameObject OneshotButtonCancel;
 
+    [Header("Custom Ambience UI")]
+    [SerializeField] private GameObject NewAmbient;
+    [SerializeField] private GameObject AmbientTitle;
+    [SerializeField] private GameObject AmbientInput;
+    [SerializeField] private GameObject AmbientButtonAudioImport;
+    [SerializeField] private GameObject AmbientButtonDone;
+    [SerializeField] private GameObject AmbientButtonCancel;
+
     private void Start()
     {
         CustomisationMenu.SetActive(false);
+
         NewOneshot.SetActive(false);
         OneshotTitle.SetActive(true);
         OneshotInput.SetActive(true);
         OneshotButtonAudioImport.SetActive(false);
         OneshotButtonDone.SetActive(false);
         OneshotButtonCancel.SetActive(true);
+
+        NewAmbient.SetActive(false);
+        AmbientTitle.SetActive(true);
+        AmbientInput.SetActive(true);
+        AmbientButtonAudioImport.SetActive(false);
+        AmbientButtonDone.SetActive(false);
+        AmbientButtonCancel.SetActive(true);
     }
 
     public void ResetCustomOneshotUI()
@@ -37,5 +53,18 @@ public class CustomisationInterface : MonoBehaviour
         OneshotButtonAudioImport.SetActive(false);
         OneshotButtonDone.SetActive(false);
         OneshotButtonCancel.SetActive(true);
+    }
+
+    public void ResetCustomAmbienceUI()
+    {
+        AmbientInput.GetComponent<TMP_InputField>().text = "";
+
+        CustomisationMenu.SetActive(false);
+        NewAmbient.SetActive(false);
+        AmbientTitle.SetActive(true);
+        AmbientInput.SetActive(true);
+        AmbientButtonAudioImport.SetActive(false);
+        AmbientButtonDone.SetActive(false);
+        AmbientButtonCancel.SetActive(true);
     }
 }
