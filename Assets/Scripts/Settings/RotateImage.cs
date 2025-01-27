522d6eff070d5daab54491ca189defbf49bb1efe
Jack Myrick <contact@jackmyrick.design>
2025-01-27 18:30:02 +0000

    Cog now only spins when active and you can click it again to toggle off settings

--- Assets/Scripts/Settings/RotateImage.cs
+++ Assets/Scripts/Settings/RotateImage.cs
@@ -17,8 +17,11 @@ namespace MusicMixer
 
         void Update()
         {
-            // Rotate around the Z-axis
-            rectTransform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+            if (gameObject.activeInHierarchy) //checks that the gameobject is active so we don't rotate it when its not being used
+            {
+                // Rotate around the Z-axis
+                rectTransform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
