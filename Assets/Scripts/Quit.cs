339d1e6c2a4206547f1ef45e751d10fa783c6d66
Jack Myrick <contact@jackmyrick.design>
2025-01-28 01:40:05 +0000

    improved Quit button

--- Assets/Scripts/Quit.cs
+++ Assets/Scripts/Quit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,7 +6,21 @@ namespace MusicMixer
 {
     public class Quit : MonoBehaviour
     {
+        [SerializeField] private AudioSource AudioSource;
+
         public void QuitApplication()
+        {
+            AudioSource.Play();
+            StartCoroutine(WaitForSFX());
+        }
+
+        private IEnumerator WaitForSFX()
+        {
+            yield return new WaitForSeconds(2);
+            ActualQuit();
+        }
+
+        private void ActualQuit()
         {
 #if UNITY_EDITOR
             EditorApplication.ExitPlaymode();
